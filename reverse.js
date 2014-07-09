8a64ead66416f318e3c050ea7ef41d3f701c9647
ncaskey04 <ncaskey04@gmail.com>
2014-07-09 12:07:29 -0700

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,5 +1,7 @@
 //Write a program that will take a hardcoded string, and console log the reverse it. Use a for loop
 
+// method 1 for reversing a string
+
 var inputString = "building";
 var isArray = inputString.split("");
 var outArray = [];
@@ -12,3 +14,17 @@ for (var i = 0; i < isArray.length; i++) {
 
 var finishedString = outArray.join("");
 console.log(finishedString);
+
+/* method 2 for reversing a string
+
+var inputString = "building";
+var outputString = "";
+
+for (var i = 0; i < inputString.length; i++){
+	outputString = inputString[i] + outputString;
+};
+
+console.log("The input word is: ", inputString);
+console.log("The reversed word is: ", outputString);
+
+*/
