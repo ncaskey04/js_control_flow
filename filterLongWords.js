db1cf703b3e593d5d160eff578e228ae28ddfa99
ncaskey04 <ncaskey04@gmail.com>
2014-07-09 12:20:50 -0700

    Update filterLongWords.js

--- filterLongWords.js
+++ filterLongWords.js
@@ -1,5 +1,7 @@
 // Hardcode an array of words. Have a variable maxLength, and push those words to only to an array filter long words
 
+// Method 1
+
 var colors = ['blue', 'red', 'yellow', 'magenta', 'chartreuse', 'pink'];
 var maxLength = 4;
 var shortColors = [];
@@ -12,3 +14,21 @@ for (var i = 0; i < colors.length; i++) {
 };
 
 console.log(shortColors);
+
+/*
+
+// Method 2
+
+var colors = ['blue', 'red', 'yellow', 'magenta', 'chartreuse', 'pink'];
+var maxLength = 4;
+var shortColors = [];
+
+for(var i = 0, currentColor; i < colors.length; i++){
+	currentColor =colors[i];
+	
+	if (currentColor.length < maxLength) {
+	  shortColors.push(currentColor);
+	}
+}
+
+*/
